26cf7b7b92aa874c98ec13bd630500b3f5116d28
Blake Lawatch <blakelawatch@gmail.com>
2023-12-11 08:24:10 -0700

    got profile by its id

--- client/src/pages/ProfilePage.vue
+++ client/src/pages/ProfilePage.vue
@@ -6,13 +6,31 @@
 
 
 <script>
-import { computed } from 'vue';
-import { AppState } from '../AppState';
+
+import { watch } from 'vue';
+import { profilesService } from '../services/ProfilesService.js'
+import Pop from '../utils/Pop';
+import { useRoute } from 'vue-router';
+
 
 
 export default {
     setup() {
+        const route = useRoute()
+        watch(() => {
+            getProfiles()
+        })
+
+        async function getProfiles() {
+            try {
+                const profileId = route.params.profileId
+                await profilesService.getProfiles(profileId)
+            } catch (error) {
+                Pop.error(error)
+            }
+        }
         return {
+
 
         }
     }
